1fc54e76ff6c793906306fa70b867ccfb0d10d4d
urieliram <ilezama.ceme@gmail.com>
2022-02-13 19:50:46 -0600

    migración a lectura de json parte 2

--- main_json.py
+++ main_json.py
@@ -1,3 +1,21 @@
+## --------------------------------------------------------------------------------
+## File: main.py
+## Unit Commitment Problem 
+## Developers: Uriel Iram Lezama Lope
+## Purpose: Programa principal de un modelo de UC
+## Description: Lee una instancia de UCP y la resuelve. 
+## Para correr el programa usar el comando "python3 main.py anjos.json thinkpad"
+## --------------------------------------------------------------------------------
+# import copy
+# import pyomo as pyo
+# from egret.models.unit_commitment import solve_unit_commitment
+# from egret.model_library.unit_commitment.uc_model_generator import UCFormulation, generate_model
+# from datetime import date
+# from datetime import datetime
+# from timeit import timeit
+# from pyomo.core.base import piecewise
+# from pyomo.core.base.boolean_var import ScalarBooleanVar
+# from pyomo.util.infeasible import log_infeasible_constraints
 import time
 import sys
 import uc_Co
@@ -15,10 +33,10 @@ import reading
 ## ferc       12    934    48   (21-24),(37-44)
 ## ferc(2)    12    978    48   (25-36)
 
-#instancia = 'archivox.json'
 instancia = 'anjos.json'
+instancia = 'archivox.json'
 ruta      = 'instances/'
-ambiente  = 'thinkpad'
+ambiente  = 'localPC'
 if ambiente == 'yalma':
     if len(sys.argv) != 3:
         print("!!! Something went wrong, try something like: $python3 main.py uc_45.json yalma")
@@ -29,17 +47,16 @@ if ambiente == 'yalma':
     instancia = sys.argv[1]
 
 localtime = time.asctime(time.localtime(time.time()))
+
 ## Append a list as new line to an old csv file
 row_file = [localtime,instancia]
 util.append_list_as_row('solution.csv', row_file)
 print(localtime, ' ',   'solving json --->', instancia)
 
-G,T,L,S,Pmax,Pmin,TU,TD,De,R,u_0,SU,SD,RU,RD,Pb,C,mpc,Cs,Tmin = reading.reading(ruta+instancia)
+##Lee instancia de archivo .json con formato Knueven2020
+G,T,L,S,Pmax,Pmin,TU,TD,De,R,u_0,U,D,SU,SD,RU,RD,pc_0,Pb,C,mpc,Cs,Tmin = reading.reading(ruta+instancia)
 
-#for i, gen in md.elements("generator", generator_type="thermal", in_service=True):  
-#    Piecewise = gen['piecewise_production']
-#    print(Piecewise)
-
+## -----------------  Caso de ejemplo se anjos.json  --------------------------
 #G        = [1, 2, 3]
 #T        = [1, 2, 3, 4, 5, 6]
 #L        = {1: [1, 2, 3], 2: [1, 2, 3], 3: [1, 2, 3, 4]}
@@ -51,27 +68,28 @@ G,T,L,S,Pmax,Pmin,TU,TD,De,R,u_0,SU,SD,RU,RD,Pb,C,mpc,Cs,Tmin = reading.reading(
 #De       = {1: 240, 2: 250, 3: 200, 4: 170, 5: 230, 6: 190}
 #R        = {1: 10, 2: 10, 3: 10, 4: 10, 5: 10, 6: 10}
 #u_0      = {1: 1, 2: 0, 3: 0}
-D        = {1: 0, 2: 0, 3: 0}
-U        = {1: 2, 2: 0, 3: 0}
+#D        = {1: 0, 2: 0, 3: 0}
+#U        = {1: 2, 2: 0, 3: 0}
 #SU       = {1: 100, 2: 70, 3: 40}
 #SD       = {1: 80, 2: 50, 3: 30}
 #RU       = {1: 50, 2: 60, 3: 70}
 #RD       = {1: 30, 2: 40, 3: 50}
-pc_0     = {1: 40, 2: 0, 3: 0}
+#pc_0     = {1: 40, 2: 0, 3: 0}
 #mpc      = {1: 400.0, 2: 750.0, 3: 900.0}
 #Pb       = {(1, 1): 80, (1, 2): 150, (1, 3): 300, (2, 1): 50, (2, 2): 100, (2, 3): 200, (3, 1): 30, (3, 2): 50, (3, 3): 70, (3, 4): 100}   
 #C        = {(1, 1): 5.0, (1, 2): 5.0, (1, 3): 5.0, (2, 1): 15.0, (2, 2): 15.0, (2, 3): 15.0, (3, 1): 30.0, (3, 2): 30.0, (3, 3): 30.0, (3, 4): 30.0}
 #Cs       = {(1, 1): 800.0, (1, 2): 800.0, (1, 3): 800.0, (2, 1): 500.0, (2, 2): 500.0, (2, 3): 500.0, (3, 1): 25.0, (3, 2): 250.0, (3, 3): 
 #500.0, (3, 4): 1000.0}
 #Tmin     = {(1, 1): 2, (1, 2): 3, (1, 3): 4, (2, 1): 2, (2, 2): 3, (2, 3): 4, (3, 1): 2, (3, 2): 3, (3, 3): 4, (3, 4): 5}
-fixShedu = False
-relax    = False
-ambiente = 'thinkpad'
-    
+#fixShedu = False
+#relax    = False
+#ambiente = 'localPC'
+## ----------------------------------  o  -------------------------------------
+
 z_exact  = 0
-t_o      = time.time()  ## Start of the calculation time count
-fixShedu = False   ## True si se fija   la solución entera, False, si se desea resolver de manera exacta
-relax    = False      ## True si se relaja la solución entera, False, si se desea resolver de manera entera    
+t_o      = time.time()  ## Start of the calculation time count.
+fixShedu = False        ## True si se fija la solución entera, False, si se desea resolver de manera exacta.
+relax    = False        ## True si se relaja la solución entera, False, si se desea resolver de manera entera.
 model    = uc_Co.uc(G,T,L,S,Pmax,Pmin,TU,TD,De,R,u_0,U,D,SU,SD,RU,RD,pc_0,mpc,
                     Pb,C,Cs,Tmin,fixShedu,relax,ambiente)
 
@@ -80,9 +98,9 @@ model    = uc_Co.uc(G,T,L,S,Pmax,Pmin,TU,TD,De,R,u_0,U,D,SU,SD,RU,RD,pc_0,mpc,
 # solver = pyo.SolverFactory('cbc')
 # https://www.ibm.com/docs/en/icos/12.8.0.0?topic=parameters-relative-mip-gap-tolerance
 solver = pyo.SolverFactory('cplex')
-if ambiente == "thinkpad":
+if ambiente == "localPC":
     solver = pyo.SolverFactory('cplex')
-if ambiente == "yalma":
+if ambiente == "yalma": 
     solver = pyo.SolverFactory('cplex', executable='/home/uriel/cplex1210/cplex/bin/x86-64_linux/cplex')
 solver.options['mip tolerances mipgap'] = 0.01  
 #solver.options['mip tolerances absmipgap'] = 200
