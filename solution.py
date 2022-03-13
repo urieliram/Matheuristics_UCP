0e3b4eda9d70e845f2eadb3fb4a4a9e83ebeba3b
urieliram <ilezama.ceme@gmail.com>
2022-03-13 14:33:40 -0600

    agregamos la clase Solution.py

---
+++ solution.py
@@ -0,0 +1,18 @@
+class Solution:
+   def __init__(self, u,v,w,p,r):
+       self.u=u
+       self.v=v
+       self.w=w
+       self.p=p
+       self.r=r
+       
+   def getU(self):
+       return self.u
+   def getV(self):
+       return self.v
+   def getW(self):
+       return self.w
+   def getP(self):
+       return self.p
+   def getR(self):
+       return self.r 
